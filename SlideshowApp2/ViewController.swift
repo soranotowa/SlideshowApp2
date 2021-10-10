6df12ffa088d091dc0b41fc048fae2624da2438b
Makoto.Noda <towaaaaaaa.s@gmail.com>
2021-10-10 22:41:54 +0900

    Initial Commit

---
+++ SlideshowApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp2
+//
+//  Created by sorano.t on 2021/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
